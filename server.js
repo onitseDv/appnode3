16df466d7311dcde3060427e823f331491817792
onitseDv <onitsedv@hotmail.com>
2022-10-26 17:19:59 -0300

    commit

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const cors = require('cors')
 const path = require ('path')
 const app = express ()
 const rotasIndex = require ('./Rotas/rotas');
-
+const rotasAuth = require ('./Rotas/rotasAuth');
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/app', express.static (path.join (__dirname, '/public')))
 app.use(rotasIndex);
+app.use('/seguranca', rotasAuth)
 
 let port = process.env.PORT || 3000
 app.listen (port, () =>{
