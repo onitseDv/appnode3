5910b31375190d91200d4e321d103ee17e4d0510
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2022-10-27 21:17:32 -0300

    Adicionado paginas

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { FormProdutosComponent } from './pages/form-produtos/form-produtos.component';
+import { ListaProdutosComponent } from './pages/lista-produtos/lista-produtos.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'produtos', pathMatch: 'full'},
+  {path: 'produtos', component: ListaProdutosComponent},
+  {path: 'form-produto', component: FormProdutosComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
