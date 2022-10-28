e86d733bea9b30caa178753d52e1779e27ecbfc6
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2022-10-28 20:22:43 -0300

    Buscando produtos da API

--- App/src/app/pages/lista-produtos/lista-produtos.component.ts
+++ App/src/app/pages/lista-produtos/lista-produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -6,12 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-produtos.component.scss']
 })
 export class ListaProdutosComponent implements OnInit {
+  produtos: any = []
 
-  constructor() { }
+  constructor(
+    private produtoService: ProdutoService
+  ) { }
 
   ngOnInit(): void {
-
+    this.buscarProdutos();
   }
+
+  buscarProdutos() {
+    this.produtoService.getProdutos().subscribe((produtos) => {
+      console.log("Produtos:", produtos)
+      this.produtos = produtos;
+    }, err => {
+      alert(err?.error?.message || "Erro ao buscar produtos! Consulte o log")
+      console.error(err);
+    })
+  }
+
   DeletarProduto(){
 
   }
